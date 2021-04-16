cb75e62943b8a330158aae40b01c9be6e5065af1
Ken <ken.museth@gmail.com>
2021-04-16 12:51:45 -0700

    forgot to sign off
    Signed-off-by: Ken <ken.museth@gmail.com>

--- camera/camera.js
+++ camera/camera.js
@@ -79,9 +79,9 @@
 		
 		thumbnails			: false,
 		
-		time				: 7000,	//milliseconds between the end of the sliding effect and the start of the nex one
+		time				: 5000,	//milliseconds between the end of the sliding effect and the start of the nex one
 		
-		transPeriod			: 1500,	//lenght of the sliding effect in milliseconds
+		transPeriod			: 1500,	//length of the sliding effect in milliseconds
 		
 ////////callbacks
 
