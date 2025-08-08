6b45a3a70529bd7ed23845bdd57f247cd1895809
Idclip <Idclip@users.noreply.github.com>
2025-08-08 05:09:28 +0000

    Automatic doxygen documentation update - Deployed from: AcademySoftwareFoundation/openvdb a64c3ef5495bd65bd92c1ddd380b5480b8ceed7b Signed-off-by: Idclip <Idclip@users.noreply.github.com>

--- documentation/doxygen/search/related_0.js
+++ documentation/doxygen/search/related_0.js
@@ -6,11 +6,11 @@ var searchData=
   ['testindexfilter',['TestIndexFilter',['../classopenvdb_1_1v12__1_1_1points_1_1RandomLeafFilter.html#ae238f13a6ebf8edcfd5ee1278d905bf2',1,'openvdb::v12_1::points::RandomLeafFilter']]],
   ['testleaf',['TestLeaf',['../classopenvdb_1_1v12__1_1_1tree_1_1LeafBuffer.html#aa1afd221f2a74a02081ab42f04511b84',1,'openvdb::v12_1::tree::LeafBuffer::TestLeaf()'],['../classopenvdb_1_1v12__1_1_1tree_1_1LeafNode.html#aa1afd221f2a74a02081ab42f04511b84',1,'openvdb::v12_1::tree::LeafNode::TestLeaf()']]],
   ['testleafio',['TestLeafIO',['../classopenvdb_1_1v12__1_1_1tree_1_1LeafNode.html#a4ec250a46a39d26ead24aff07632e82b',1,'openvdb::v12_1::tree::LeafNode']]],
-  ['testlogger',['TestLogger',['../classopenvdb_1_1v12__1_1_1ax_1_1Logger.html#ae37f3ca373fc88722ea49a22f5446169',1,'openvdb::v12_1::ax::Logger']]],
+  ['testloggeracc',['TestLoggerAcc',['../classopenvdb_1_1v12__1_1_1ax_1_1Logger.html#ad18aadd1014f76d68b70ca55e3778857',1,'openvdb::v12_1::ax::Logger']]],
   ['testpointdataleaf',['TestPointDataLeaf',['../classopenvdb_1_1v12__1_1_1points_1_1PointDataLeafNode.html#ac1bb4f6424e526be4f0fa9e631e84cd5',1,'openvdb::v12_1::points::PointDataLeafNode']]],
-  ['testpointexecutable',['TestPointExecutable',['../classopenvdb_1_1v12__1_1_1ax_1_1PointExecutable.html#adfeb0ea9d4bda2e4c2a4395916b156c3',1,'openvdb::v12_1::ax::PointExecutable']]],
+  ['testpointexecutableacc',['TestPointExecutableAcc',['../classopenvdb_1_1v12__1_1_1ax_1_1PointExecutable.html#afb134b0cfe04a4d2d8e5456ef156cd70',1,'openvdb::v12_1::ax::PointExecutable']]],
   ['testpointmove',['TestPointMove',['../classopenvdb_1_1v12__1_1_1points_1_1CachedDeformer.html#aa110e4d1e92a113e6e806b89b0ed1bbb',1,'openvdb::v12_1::points::CachedDeformer']]],
   ['teststream',['TestStream',['../classopenvdb_1_1v12__1_1_1io_1_1File.html#a44f87839bb17db5d4f50b1cc375a2c37',1,'openvdb::v12_1::io::File']]],
   ['teststreamcompression',['TestStreamCompression',['../classopenvdb_1_1v12__1_1_1compression_1_1PageHandle.html#ac0d4b31e609900e9fd00b39cb8863a16',1,'openvdb::v12_1::compression::PageHandle']]],
-  ['testvolumeexecutable',['TestVolumeExecutable',['../classopenvdb_1_1v12__1_1_1ax_1_1VolumeExecutable.html#af8da9c82256fc211311bff5dfcdc1525',1,'openvdb::v12_1::ax::VolumeExecutable']]]
+  ['testvolumeexecutableacc',['TestVolumeExecutableAcc',['../classopenvdb_1_1v12__1_1_1ax_1_1VolumeExecutable.html#a05bfde7168d80e33821dfb0a9d85ea09',1,'openvdb::v12_1::ax::VolumeExecutable']]]
 ];
